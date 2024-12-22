6b3b1c7aa1d4371592ff7590a2065ee4d166478f
Jeel Pansuriya <jeelishwa@gmail.com>
2024-12-23 01:16:26 +0530

    setup

---
+++ Server/src/models/pet.model.js
@@ -0,0 +1,52 @@
+import mongoose,{Schema} from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const petSchema = new Schema(
+    {
+        petName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        petType: {
+            type: String,
+            enum: {values: ['Dog', 'Cat', 'Bird', 'Fish', 'Other']},
+            required: true,
+            trim: true
+        },
+        breed: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        age: {
+            type: Number,
+            required: true,
+            trim: true
+        },
+        gender: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        image: {
+            type: String, //cloudinary url
+            required: true,
+            trim: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    },
+    {
+        timestamp: true
+    }
+)
